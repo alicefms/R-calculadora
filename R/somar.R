f7c0876267c4a52449383e7bee2ddc0ac4d33db1
Alice Souza <alicefms@hotmail.com>
2021-11-12 20:09:38 -0300

    soma e subtrai

---
+++ R/somar.R
@@ -0,0 +1,15 @@
+#'Função para somar
+#'
+#'Esta função serve para somar dois numeros
+#'
+#'@param a
+#'@param b
+#'
+#'@exemples
+#' somar(2,3)
+#'--> 5
+#' @export
+somar <- function(a,b){
+  a+b
+}
+
---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#'Função para subtrair
+#'
+#'Esta função serve para subtrair dois numeros
+#'
+#'@param a
+#'@param b
+#'
+#'@exemples
+#' subtrair(3,2)
+#'--->1
+#' @export
+subtrair <- function(a,b){
+  a-b
+}
